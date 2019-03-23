9f003b6ee66b24afee2f8c82520a70c5cbd7e448
Cem Bozsahin <cem.bozsahin@gmail.com>
2019-03-23 15:43:02 +0300

    printing text and attrib

---
+++ xml-traverse.py
@@ -0,0 +1,25 @@
+# Depth first or breadth first traversal of an xml file of any depth and breadth 
+# --cem bozsahin, Mar 2019
+
+# argument index 0: this script, index 1: input file, 2: d or b
+
+import sys # for interface
+import xml.etree.ElementTree as ET   # for xml processing
+
+def xml-recurse (rt):
+    agenda=[]
+    for elm in rt:
+        if sys.argv[2] == "d":
+            agenda.append(elm)
+        else if sys.argv[2] == "b":
+            agenda.insert(0,elm)
+        else agenda.append(elm)  # default is depth first
+    for a in agenda:
+        print("tag: %s  text: %s" % (a.attrib,a.text))
+        xml-recurse(a)
+
+
+# get the xml file's structured representation
+tree = ET.parse(sys.argv[1])
+root = tree.getroot()
+xml-recurse(root)
