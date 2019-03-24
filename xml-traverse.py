8dafdbe48526c3b1c8c59dce960794cb7b5a63c8
Cem Bozsahin <cem.bozsahin@gmail.com>
2019-03-24 17:43:40 +0300

    trying to get prints in the right order

--- xml-traverse.py
+++ xml-traverse.py
@@ -6,22 +6,24 @@
 import sys # for interface
 import xml.etree.ElementTree as ET   # for xml processing
 
-def xml_recurse (rt):
+tmode="d"
+if sys.argv[2] == "b":
+    tmode="b"
+def xml_recurse (rt,tmode):
     agenda=[]
     for elm in rt:
-        if sys.argv[2] == "d":
+        if tmode == "d":
             agenda.append(elm)
-        elif sys.argv[2] == "b":
-            agenda.insert(0,elm)
         else:
-            agenda.append(elm)  # default is depth first
-    for a in agenda:
-        print("tag: %s  text: %s" % (a.attrib,a.text))
-        xml_recurse(a)
+            agenda.insert(0,elm)
+    for a in reversed(agenda):  # ET seems to append everything: process in reverse
+        print("tag: %s\n  attrib:  %s\n  text: %s" % (a.tag,a.attrib,a.text))
+        xml_recurse(a,tmode)
 
 
 # get the xml file's structured representation
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
+print("Traversing file %s in %s mode:\n--------------" % (sys.argv[1],tmode))
 # fire away
-xml_recurse(root)
+xml_recurse(root,tmode)
